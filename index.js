e750e2580e05bd5f91df548250ee604f4c9de9f1
ghufran <ghufran@ghufran.com>
2025-03-08 14:32:59 +0530

    links added

---
+++ index.js
@@ -0,0 +1,9 @@
+$(".bi-github").on("click",function () {
+  window.open("https://github.com/ghufran-haider") ;
+});
+$(".bi-instagram").on("click",function () {
+    window.open("https://www.instagram.com/ghufran.haider_naqvi/?next=https%3A%2F%2Fwww.instagram.com%2Fdirect%2Ft%2F102970187769291%2F%3F__coig_login%3D1");
+})
+$(".bi-twitter-x").on("click",function () {
+    window.open("https://x.com/ghufranir?t=NbNt6cSQcl0aKSigSytAfg&s=08");
+})
